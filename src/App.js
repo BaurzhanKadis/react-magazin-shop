f0284428c4fc203aa758703a2e27eb79cf6a4de9
BaurzhanKadis <37457582+BaurzhanKadis@users.noreply.github.com>
2019-05-28 22:43:02 +0300

    второй пуш

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello world!!!
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-6">One</div>
+          <div className="col-6">
+            <span>
+              <i className='fas fa-home'></i>
+            </span>
+          </div>
+        </div>
+        Hello world!!!
+      </div>
+    );
+  } 
 }
-
-export default App;
