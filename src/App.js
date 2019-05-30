e33c498f318df82f712f82e6a481b6c33324be58
BaurzhanKadis <37457582+BaurzhanKadis@users.noreply.github.com>
2019-05-31 02:39:57 +0300

    4 push

--- src/App.js
+++ src/App.js
@@ -7,18 +7,21 @@ import ProductList from './components/ProductList';
 import Details from './components/Details';
 import Cart from './components/Cart';
 import Default from './components/Default';
+import Modal from './components/Modal';
+
 
 export default class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Navbar />
-          <Switch>
-            <Route exact path="/" component={ProductList}/>
-            <Route path="/details" component={Details}/>
-            <Route path="/cart" component={Cart}/>
-            <Route component={Default}/>
-          </Switch>
+        <Switch>
+          <Route exact path="/" component={ProductList}/>
+          <Route path="/details" component={Details}/>
+          <Route path="/cart" component={Cart}/>
+          <Route component={Default}/>
+        </Switch>
+        <Modal />
       </React.Fragment>
     );
   } 
